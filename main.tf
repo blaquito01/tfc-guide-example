fdfb09321bd4a4c004415ec69a4ee0e49d05d893
Jones <Jeremy.Jones@thomsonreuters.com>
2020-06-09 14:15:13 -0500

    first commit

--- main.tf
+++ main.tf
@@ -1,28 +1,81 @@
+# PROVIDERS
+##################################################################################
+
 provider "aws" {
-  version = "2.33.0"
-
-  region = var.aws_region
+  access_key = "AKIAW72NFG3WRZEMITNJ"
+  secret_key = "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"
+  region = "us-east-1"
 }
 
-provider "random" {
-  version = "2.2"
+##################################################################################
+# DATA
+##################################################################################
+
+data "aws_ami" "aws-linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
-resource "random_pet" "table_name" {}
 
-resource "aws_dynamodb_table" "tfc_example_table" {
-  name = "${var.db_table_name}-${random_pet.table_name.id}"
+##################################################################################
+# RESOURCES
+##################################################################################
 
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
+#This uses the default VPC.  It WILL NOT delete it on destroy.
+resource "aws_default_vpc" "default" {
 
-  attribute {
-    name = "UUID"
-    type = "S"
+}
+resource "aws_security_group" "allow_ssh" {
+  name        = "nginx_demo"
+  description = "Allow ports for nginx demo"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = {
-    user_name = var.tag_user_name
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "nginx" {
+  ami                    = data.aws_ami.aws-linux.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+}
+
+##################################################################################
+# OUTPUT
+##################################################################################
+
+output "aws_instance_public_dns" {
+  value = aws_instance.nginx.public_dns
 }
